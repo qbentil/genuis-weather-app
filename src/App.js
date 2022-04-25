fc0b81d0866b93a7710c554eadfd64d5d2dd1b66
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-04-25 22:46:01 +0000

    Change default template text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -7,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Team Genius Weather App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
