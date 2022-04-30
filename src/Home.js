ab2f73e2004d718998fc21a3a1ee395e8daeb8b4
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-04-30 15:20:19 +0000

    Update

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,11 @@
 import './App.css';
+
 import SearchBar from './components/SearchBar';
-
-
-import logo from './logo.svg';
 
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <SearchBar />
-        <p>
-          Welcome to Team Genius Weather App
-        </p>
+    <div className="body">
 
-      </header>
     </div>
   );
 }
