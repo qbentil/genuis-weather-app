2998b591d93c01c570dfe8713fa0272c63797093
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-01 16:01:15 +0000

    Restrucdture Searchbar component

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+const SearchBar =() =>{
+    return(
+        <div className="container">
+            <div className='icon'><p>Hi</p></div>
+            <input type='text' placeholder="Enter City to search" />
+            <div className='icon'><hi>Hi</hi></div>
+        </div>
+    )
+}
+
+export default SearchBar;
