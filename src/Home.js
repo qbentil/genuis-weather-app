83dda392fe941538c0990ab29505396d2165b5e8
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-01 16:52:22 +0000

    Implement SearchBar prop drilling

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,12 @@ import SearchBar from './components/SearchBar';
 import { Waether } from './components/Weather';
 
 function Home() {
+  const searchWeather = (location) => {
+    // alert(location)
+  }
   return (
     <div className="body">
-        <SearchBar />
+        <SearchBar action = {searchWeather} />
         <Waether />
     </div>
   );
