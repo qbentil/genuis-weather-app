6957fca77a80bb0423cabcd02df62f9081235299
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-01 16:52:40 +0000

    Include Search Icons in searchBar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,11 +1,24 @@
 import './style.css'
 
-const SearchBar =() =>{
+import { FaSearchLocation } from 'react-icons/fa';
+import {MdOutlineLocationSearching} from 'react-icons/md'
+import { useState } from 'react';
+
+const SearchBar =({action}) =>{
+    const [location, setLocation] = useState();
+    const handleSearch = (e) => {
+        setLocation(e.target.value);
+        action(location)
+    }
     return(
         <div className="container">
-            <div className='icon'><p>Hi</p></div>
-            <input type='text' placeholder="Enter City to search" />
-            <div className='icon'><hi>Hi</hi></div>
+            <div className='icon'>
+            <FaSearchLocation   />
+            </div>
+            <input type='text' value={location} placeholder="Enter City to search" onChange={(e) => handleSearch(e)} />
+            <div className='icon'>
+                <MdOutlineLocationSearching />
+            </div>
         </div>
     )
 }
