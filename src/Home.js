662452f8a14b2738215f73eb670a87d3b53c3430
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-06 13:14:22 +0000

    Complete Home APP

--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,50 @@
 import './Home.css';
 
-import SearchBar from './components/SearchBar';
-import { Waether } from './components/Weather';
-import axios from 'axios'
+import Search from './components/Search';
+import { Weather } from './components/Weather';
+import axios from "axios";
 import { useState } from 'react';
 
 function Home() {
-  const [data, setData] = useState({})
+  const [getDefault, setGetDefault] = useState(true)
+  const [data, setData] = useState();
   const searchWeather = async (location) => {
-    let url = "https://community-open-weather-map.p.rapidapi.com/weather"
+    setGetDefault(false)
+    let url = "https://community-open-weather-map.p.rapidapi.com/weather";
     const options = {
-        params: {
-          q: location,
-          lang: 'null',
-          units: 'imperial',
-        },
-        headers: {
-          'X-RapidAPI-Host': process.env.REACT_APP_API_HOST,
-          'X-RapidAPI-Key': process.env.REACT_APP_API_KEY
-        }
+      params: {
+        q: location,
+        lang: "null",
+        units: "imperial",
+      },
+      headers: {
+        "X-RapidAPI-Host": process.env.REACT_APP_API_HOST,
+        "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+      },
     };
 
     try {
-        let res = await axios.get(url, options)
-        let data = await res.data
-        setData(data)
+      let res = await axios.get(url, options);
+      let data = await res.data;
+      setData(data);
     } catch (e) {
-        console.log(e.response.data.message);
-        return;
+      console.log(e.response.data.message);
+      return;
     }
+  };
+  
+  // Fetch Default Data
+  if(getDefault)
+  {
+    searchWeather("Accra")
   }
   return (
-    <div className="body">
-        <SearchBar action = {searchWeather} />
-        <Waether />
+    <div className="w-screen h-screen bg-gray-600 items-center flex justify-center">
+
+      <div className='w-[80%] h-[80%] bg-white mx-auto items-center flex  rounded-2xl'>
+          <Search action = {searchWeather} />
+          <Weather data = {data} />
+      </div>
     </div>
   );
 }
