6dfb6aa09db76eaff921bd305ed54d50bdb94db2
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-06 13:14:10 +0000

    Update TW config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'black-rgba': 'rgba(0, 0, 0, 0.25)',
+      },
+      fontFamily: {
+        'poppins': ['Poppins'],
+      }
+    },
+  },
+  plugins: [],
+}
